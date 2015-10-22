f91a0ccc53e92ced95b6295ad2ce0400695d80a7
Z Snow <rossetti@Waka.local>
2015-10-23 07:03:41 +1100

    ldap_user_conf_admin is a property.

--- ldap_user/src/Form/LdapUserAdminForm.php
+++ ldap_user/src/Form/LdapUserAdminForm.php
@@ -13,6 +13,12 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class LdapUserAdminForm extends ConfigFormBase {
 
+  public static $ldap_user_conf_admin = NULL;
+
+  public function __construct() {
+    parent::__construct();
+    self::$ldap_user_conf_admin = ldap_user_conf('admin');
+  }
   /**
    * {@inheritdoc}
    */
@@ -28,15 +34,12 @@ class LdapUserAdminForm extends ConfigFormBase {
   }
 
   public function buildForm(array $form, \Drupal\Core\Form\FormStateInterface $form_state) {
-    $ldap_user_conf_admin = ldap_user_conf('admin');
-    $form = $ldap_user_conf_admin->drupalForm();
+    $form = self::$ldap_user_conf_admin->drupalForm();
     return $form;
   }
 
   public function validateForm(array &$form, \Drupal\Core\Form\FormStateInterface $form_state) {
-
-    $ldap_user_conf_admin = ldap_user_conf('admin');
-    list($errors, $warnings) = $ldap_user_conf_admin->drupalFormValidate($form_state->getValues(), $form['#storage']);
+    list($errors, $warnings) = self::$ldap_user_conf_admin->drupalFormValidate($form_state->getValues(), $form['#storage']);
     foreach ($errors as $error_name => $error_text) {
       $form_state->setErrorByName($error_name, t($error_text));
     }
@@ -47,19 +50,16 @@ class LdapUserAdminForm extends ConfigFormBase {
   }
 
   public function submitForm(array &$form, \Drupal\Core\Form\FormStateInterface $form_state) {
+    self::$ldap_user_conf_admin->drupalFormSubmit($form_state->getValues(), $form['#storage']); // add form data to object and save or create
 
-    $ldap_user_conf_admin = ldap_user_conf('admin');
-    $ldap_user_conf_admin->drupalFormSubmit($form_state->getValues(), $form['#storage']); // add form data to object and save or create
-
-    if ($ldap_user_conf_admin->hasError == FALSE) {
+    if (self::$ldap_user_conf_admin->hasError == FALSE) {
       drupal_set_message(t('LDAP user configuration saved'), 'status');
       return new RedirectResponse(\Drupal::url('ldap_user.admin_form'));
     }
     else {
       $form_state->setErrorByName($conf->errorName, $conf->errorMsg);
-      $ldap_user_conf_admin->clearError();
+      self::$ldap_user_conf_admin->clearError();
     }
-
   }
 
 }
