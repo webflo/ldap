246d04ac4b5368b27655e2a2a9ede20f76b346b5
Mr Snow <snow@hol.ly>
2015-11-02 12:43:37 +1100

    Correctly working subscriber to replace hook_init.

--- ldap_authentication/src/EventSubscriber/LDAPAuthenticationSubscriber.php
+++ ldap_authentication/src/EventSubscriber/LDAPAuthenticationSubscriber.php
@@ -1,6 +1,6 @@
 <?php /**
  * @file
- * Contains \Drupal\ldap_authentication\EventSubscriber\InitSubscriber.
+ * Contains \Drupal\ldap_authentication\EventSubscriber\LDAPAuthenticationSubscriber.
  */
 
 namespace Drupal\ldap_authentication\EventSubscriber;
@@ -8,20 +8,21 @@ namespace Drupal\ldap_authentication\EventSubscriber;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
-class InitSubscriber implements EventSubscriberInterface {
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function getSubscribedEvents() {
-    return [KernelEvents::REQUEST => ['onEvent', 0]];
-  }
+class LDAPAuthenticationSubscriber implements EventSubscriberInterface {
 
   public function onEvent() {
     $auth_conf = ldap_authentication_get_valid_conf();
     if ($auth_conf && $auth_conf->templateUsagePromptUser) {
       ldap_authentication_check_for_email_template();
     }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  static function getSubscribedEvents() {
+    $events[KernelEvents::REQUEST][] = array('onEvent');
+    return $events;
   }
 
 }
